afb99117ec153bb78516476bf3b66a64e8636d95
Luis Cagigal <60861089+LuchoC976@users.noreply.github.com>
2022-04-05 21:38:38 -0500

    QRCode maker with Python and Tkinter

---
+++ QRCodeMaker/QRCodeTkinter.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from tkinter import *
+import qrcode
+import validators
+
+'''
+QRCode Maker Python implementation using qrcode module
+and TKinter as the GUI
+'''
+
+# Tkinter main window
+window = tk.Tk()
+window.geometry("300x200")
+window.title("QRCode Maker")
+
+window.configure(background = "blue")
+
+# Default URL -> Google.com
+defaultURL = "https://www.google.com/"
+
+# getURL function -> validates URL
+def getURL():
+	url = urlBox.get()
+	if len(url) == 0:
+		url = defaultURL
+	if not validators.url(url):
+		url = defaultURL
+
+	# makeQR function called
+	makeQR(url)
+
+# makeQR function -> uses qrcode module to make qrcode for a URL
+def makeQR(url):
+	imgQR = qrcode.make(url)
+	imgQR.show()
+
+	# Save IMG chackbox **NOTE: Saving a new img will overwrite the last one**
+	if saveImg.get() == 1:
+		imgQR.save("MyQRCode")
+
+# Title (Bold, 18px size)
+title = tk.Label(window, text = "Convert URL into QRCode:", bg = "blue", 
+	fg = "white", pady = 10)
+title.configure(font="helvetica 18 bold")
+title.pack()
+
+# Prompt
+prompt = tk.Label(window, text = "Enter URL:", bg = "blue", 
+	fg = "white", pady = 10)
+prompt.pack()
+
+# URL string box
+urlBox = tk.Entry(window, bg = "blue", fg = "white")
+urlBox.pack()
+
+# saveImg -> Checkbutton status validator (selected or not selected)
+saveImg = IntVar()
+tk.Checkbutton(window, text="Save QRCode", variable = saveImg, pady = 10, bg = "blue", 
+	fg = "white", onvalue = 1, offvalue = 0).pack()
+
+# Saves URL and calls getURL function
+makeQRBtn = tk.Button(window, text = "Show URL", command = getURL, 
+	highlightbackground = "blue", pady = 30)
+makeQRBtn.pack()
+
+# Starts tk
+window.mainloop()
+
---
+++ QRCodeMaker/README.md
@@ -0,0 +1,13 @@
+This QRCode maker is made with the TKinter python module for GUI, 
+it also uses de qrcode module form python as well, basically you
+enter a URL into the box and the system verifies that is a genuine
+URL, if it is, then it will show you the QRCode for that URL in a
+new displayed window; if it is not, then the URL will be set to
+a default URL, which is https://google.com
+If you click on the checkbox to save the image of the QRCode, then
+it will automatically be saved in your computer. 
+The only issue with this feature is that it can only save 1 image
+at a time, unless you change the names of the files, each time you
+save a new image, the system will overwrite the previous saved image
+But apart from that, it works fine
+- L
